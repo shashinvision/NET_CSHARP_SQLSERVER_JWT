a7539236760673a9081bbfad72696cc1a487aed0
Felipe Mancilla <felipei.mancilla@gmail.com>
2024-09-09 11:32:06 -0300

    Add Layout

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -3,15 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import {
   SidebarComponent
 } from './layout/sidebar/sidebar.component';
-import { ContentComponent
-} from './layout/content/content.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { NavComponent } from './layout/nav/nav.component';
 import { ControlComponent } from './layout/control/control.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , SidebarComponent, ContentComponent, FooterComponent, NavComponent, ControlComponent],
+  imports: [RouterOutlet , SidebarComponent, FooterComponent, NavComponent, ControlComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
